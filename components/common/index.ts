346bee483f8e58b9f1c5e087ba7b0c0e55620628
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2020-09-03 00:49:16 +0800

    Remove all

--- components/common/index.ts
+++
@@ -1,8 +0,0 @@
-import Text from './Text'
-import Heading from './Heading'
-import Anchor from './Anchor'
-
-const modules = { Text, Heading, Anchor }
-
-export { Text, Heading, Anchor }
-export default modules
--- next-env.d.ts
+++
@@ -1,3 +0,0 @@
-/* eslint spaced-comment: ["error", "always", { "markers": ["/"] }] */
-/// <reference types="next" />
-/// <reference types="next/types/global" />
