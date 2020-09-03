c4f607b8187778f1b4adf4fe4b1d4167d8b81260
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2020-09-03 12:13:19 +0800

    Setup Styled Components and tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
 import { AppProps } from 'next/app'
+import styled from 'styled-components'
+
+import 'styles/tailwind.css'
+
+const AppWrapper = styled.div`
+  grid-template-rows: auto 1fr auto;
+`
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <AppWrapper className="grid min-h-screen">
+      <header>header</header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <footer>footer</footer>
+    </AppWrapper>
+  )
 }
 
 export default App
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+  },
+  variants: {},
+  plugins: [],
+}
