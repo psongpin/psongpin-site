ab962f66302d909806e12062084b18273eb4c69f
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2021-09-16 12:42:08 +0800

    chore: clean-up initial files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MainApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
 }
-export default MyApp
+export default MainApp
