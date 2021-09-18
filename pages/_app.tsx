3644bf80a2ab19a3b13e2b19221c5146ea687499
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2021-09-18 14:54:35 +0800

    chore: stitches setup for css reset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from 'next/app'
 
+import globalStyles from 'utils/styles/reset'
+
 const MainApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  globalStyles()
+
   return <Component {...pageProps} />
 }
 export default MainApp
---
+++ utils/styles/reset.ts
@@ -0,0 +1,6 @@
+import { globalCss } from 'utils/styles/stitches.config'
+import { reset } from 'stitches-normalize-css'
+
+const globalStyles = globalCss(reset)
+
+export default globalStyles
