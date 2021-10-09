698ea26d10c4f45d4dfd49b33cf605861ca8c5a9
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2021-10-09 08:21:47 +0800

    style: add montserat

--- utils/styles/fonts.ts
+++ utils/styles/fonts.ts
@@ -1,5 +1,5 @@
 const sans =
-  'ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
+  '"Montserrat", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
 const serif = 'ui-serif, Georgia, Cambria, "Times New Roman", Times, serif'
 const mono =
   'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace'
