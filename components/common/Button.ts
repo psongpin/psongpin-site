e9fe269ad0a191647a966a2e94c754f28507ec3e
Paul Simon Ongpin <paulsimon.ongpin@gmail.com>
2021-10-09 11:28:36 +0800

    feat: button, container and flex common components

---
+++ components/common/Button.ts
@@ -0,0 +1,57 @@
+import { styled } from 'utils/styles/stitches.config'
+
+const Button = styled('div', {
+  display: 'inline-flex',
+  flexCenter: true,
+  borderRadius: '$base',
+  fontWeight: '$medium',
+  border: '1px solid',
+  color: '$slateLowContrastText',
+  transition: '$mediumEaseInOut',
+
+  variants: {
+    btnType: {
+      solid: {
+        backgroundColor: '$slateElemBg',
+        borderColor: '$slateElemBg',
+        '&:hover': {
+          backgroundColor: '$slateElemHoverBg',
+        },
+        '&:active': {
+          backgroundColor: '$slateElemActiveBg',
+        },
+      },
+      outlined: {
+        backgroundColor: '$slateAppBg',
+        borderColor: '$slateBorderColor',
+        '&:hover, &:active': {
+          borderColor: '$slateBorderHoverColor',
+        },
+      },
+    },
+    size: {
+      small: {
+        height: 28,
+        px: '$3',
+        fontSize: '$sm',
+      },
+      normal: {
+        height: 38,
+        px: '$4',
+        fontSize: '$base',
+      },
+      large: {
+        height: 48,
+        px: '$5',
+        fontSize: '$lg',
+      },
+    },
+  },
+
+  defaultVariants: {
+    btnType: 'solid',
+    size: 'normal',
+  },
+})
+
+export default Button
---
+++ components/common/Flex.ts
@@ -0,0 +1,7 @@
+import { styled } from 'utils/styles/stitches.config'
+
+const Flex = styled('div', {
+  display: 'flex',
+})
+
+export default Flex
